446983c6824c547caa0b694a20397e9644ad09fe
chae401 <chae401@sju.ac.kr>
2023-11-17 17:58:11 +0900

    feat: set websocket

--- src/main/java/com/coffeetree/consumer/Container.java
+++ src/main/java/com/coffeetree/consumer/Container.java
@@ -1,2 +1,7 @@
-package com.coffeetree.consumer;public class Container {
+package com.coffeetree.consumer;
+
+public class Container {
+    private Long id;
+    private double location_x;
+    private double location_y;
 }
