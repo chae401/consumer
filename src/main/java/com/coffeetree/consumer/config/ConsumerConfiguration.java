a34e72acd497af98900fdabead21464e10b7a482
chae401 <chae401@sju.ac.kr>
2023-11-19 18:18:25 +0900

    edit

--- src/main/java/com/coffeetree/consumer/config/ConsumerConfiguration.java
+++ src/main/java/com/coffeetree/consumer/config/ConsumerConfiguration.java
@@ -41,7 +41,7 @@ public class ConsumerConfiguration {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         props.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaProperties.CONSUMER_GROUP);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return props;
     }
